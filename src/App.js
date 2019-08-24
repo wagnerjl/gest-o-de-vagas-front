e29f84ba78600cc4b30158a846afb6838bf0b27c
Patrik Picoli <ppicoli@ciandt.com>
2019-08-24 12:43:29 -0300

    Header component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 
+import Header from './components/Header'
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+    <div className="App container-fluid">
+      <Header />
     </div>
   );
 }
