5ee850a2d62a5143b9dacc0fbab72e75a43c07e5
Patrik Picoli <ppicoli@ciandt.com>
2019-08-24 12:47:18 -0300

    Component consulta

---
+++ src/components/Consulta.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+export default class Consulta extends Component {
+  render() {
+    return (
+      <div class="container">
+            <div class="row col-md-12 text-center">
+                <div class="alert alert-primary col-md-4 center-block" role="alert">
+                    Valide se o carro possui permissão
+                </div>
+            </div>
+            <div>
+                <div style="background-color: #57636e; width: 300px; height: 300px; border-radius: 25px;">
+                    <div style="padding-top: 40%; text-align: center; vertical-align: center; vertical-align: middle; color: white; font-size: 30px">TIRAR FOTO</div>
+                </div>
+            </div>
+        </div>
+    )
+  }
+}
